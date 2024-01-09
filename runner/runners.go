8d57e81fb393092017f64cd45dda4b646ad4d184
zouyuxuan <zouyuxuancode@163.com>
2024-01-09 01:39:52 -0800

    add tar.gz DeCompress

---
+++ runner/runners.go
@@ -0,0 +1,481 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package runner
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/buildpacks/libcnb"
+	"github.com/mattn/go-shellwords"
+	"github.com/paketo-buildpacks/libpak"
+	"github.com/paketo-buildpacks/libpak/bard"
+	"github.com/paketo-buildpacks/libpak/effect"
+	"github.com/paketo-buildpacks/libpak/sherpa"
+)
+
+//go:generate mockery --name ScarbService --case underscore
+
+type ScarbService interface {
+	Install(srcDir string, destLayer libcnb.Layer) error
+	InstallMember(memberPath string, srcDir string, destLayer libcnb.Layer) error
+	InstallTool(name string, additionalArgs []string) error
+	WorkspaceMembers(srcDir string, destLayer libcnb.Layer) ([]url.URL, error)
+	ProjectTargets(srcDir string) ([]string, error)
+	CleanScarbHomeCache() error
+	ScarbVersion() (string, error)
+}
+
+const (
+	StaticTypeMUSLC   = "muslc"
+	StaticTypeGNULIBC = "gnulibc"
+)
+
+// Option is a function for configuring a ScarbRunner
+type Option func(runner ScarbRunner) ScarbRunner
+
+// WithScarbHome sets CARGO_HOME
+func WithScarbHome(cargoHome string) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.ScarbHome = cargoHome
+		return runner
+	}
+}
+
+// WithScarbWorkspaceMembers sets a comma separate list of workspace members
+func WithScarbWorkspaceMembers(cargoWorkspaceMembers string) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.ScarbWorkspaceMembers = cargoWorkspaceMembers
+		return runner
+	}
+}
+
+// WithScarbInstallArgs sets addition args to pass to cargo install
+func WithScarbInstallArgs(installArgs string) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.ScarbInstallArgs = installArgs
+		return runner
+	}
+}
+
+// WithExecutor sets the executor to use when running cargo
+func WithExecutor(executor effect.Executor) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.Executor = executor
+		return runner
+	}
+}
+
+// WithLogger sets additional args to pass to cargo install
+func WithLogger(logger bard.Logger) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.Logger = logger
+		return runner
+	}
+}
+
+// WithStack sets the stack on which we're running
+func WithStack(stack string) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.Stack = stack
+		return runner
+	}
+}
+
+// WithStaticType sets the static type to use
+func WithStaticType(staticType string) Option {
+	return func(runner ScarbRunner) ScarbRunner {
+		runner.StaticType = staticType
+		return runner
+	}
+}
+
+// ScarbRunner can execute cargo via CLI
+type ScarbRunner struct {
+	ScarbHome             string
+	ScarbWorkspaceMembers string
+	ScarbInstallArgs      string
+	Executor              effect.Executor
+	Logger                bard.Logger
+	Stack                 string
+	StaticType            string
+}
+
+type metadataTarget struct {
+	Kind       []string `json:"kind"`
+	CrateTypes []string `json:"crate_types"`
+	Name       string   `json:"name"`
+	SrcPath    string   `json:"src_path"`
+	Edition    string   `json:"edition"`
+	Doc        bool     `json:"doc"`
+	Doctest    bool     `json:"doctest"`
+	Test       bool     `json:"test"`
+}
+
+type metadataPackage struct {
+	ID      string
+	Targets []metadataTarget `json:"targets"`
+}
+
+type metadata struct {
+	Packages         []metadataPackage `json:"packages"`
+	WorkspaceMembers []string          `json:"workspace_members"`
+}
+
+// NewScarbRunner creates a new cargo runner with the given options
+func NewScarbRunner(options ...Option) ScarbRunner {
+	runner := ScarbRunner{}
+
+	for _, option := range options {
+		runner = option(runner)
+	}
+
+	return runner
+}
+
+// Install will build and install the project using `cargo install`
+func (c ScarbRunner) Install(srcDir string, destLayer libcnb.Layer) error {
+	return c.InstallMember(".", srcDir, destLayer)
+}
+
+// InstallMember will build and install a specific workspace member using `cargo install`
+func (c ScarbRunner) InstallMember(memberPath string, srcDir string, destLayer libcnb.Layer) error {
+	// makes warning from `cargo install` go away
+	path := os.Getenv("PATH")
+	if path != "" && !strings.Contains(path, destLayer.Path) {
+		path = sherpa.AppendToEnvVar("PATH", ":", filepath.Join(destLayer.Path, "bin"))
+		err := os.Setenv("PATH", path)
+		if err != nil {
+			return fmt.Errorf("unable to update PATH\n%w", err)
+		}
+	}
+
+	args, err := c.BuildArgs(destLayer, memberPath)
+	if err != nil {
+		return fmt.Errorf("unable to build args\n%w", err)
+	}
+
+	c.Logger.Bodyf("cargo %s", strings.Join(args, " "))
+	if err := c.Executor.Execute(effect.Execution{
+		Command: "cargo",
+		Args:    args,
+		Dir:     srcDir,
+		Stdout:  bard.NewWriter(c.Logger.Logger.InfoWriter(), bard.WithIndent(3)),
+		Stderr:  bard.NewWriter(c.Logger.Logger.InfoWriter(), bard.WithIndent(3)),
+	}); err != nil {
+		return fmt.Errorf("unable to build\n%w", err)
+	}
+
+	err = c.CleanScarbHomeCache()
+	if err != nil {
+		return fmt.Errorf("unable to cleanup: %w", err)
+	}
+	return nil
+}
+
+func (c ScarbRunner) InstallTool(name string, additionalArgs []string) error {
+	args := []string{"install", name}
+	args = append(args, additionalArgs...)
+
+	c.Logger.Bodyf("cargo %s", strings.Join(args, " "))
+	if err := c.Executor.Execute(effect.Execution{
+		Command: "cargo",
+		Args:    args,
+		Stdout:  bard.NewWriter(c.Logger.Logger.InfoWriter(), bard.WithIndent(3)),
+		Stderr:  bard.NewWriter(c.Logger.Logger.InfoWriter(), bard.WithIndent(3)),
+	}); err != nil {
+		return fmt.Errorf("unable to install tool\n%w", err)
+	}
+
+	return nil
+}
+
+// WorkspaceMembers loads the members from the project workspace
+func (c ScarbRunner) WorkspaceMembers(srcDir string, destLayer libcnb.Layer) ([]url.URL, error) {
+	m, err := c.fetchScarbMetadata(srcDir)
+	if err != nil {
+		return []url.URL{}, fmt.Errorf("unable to load cargo metadata\n%w", err)
+	}
+
+	filterMap := c.makeFilterMap()
+
+	var paths []url.URL
+	for _, workspace := range m.WorkspaceMembers {
+		// This is OK because the workspace member format is `package-name package-version (url)` and
+		//   none of name, version or URL may contain a space & be valid
+		parts := strings.SplitN(workspace, " ", 3)
+		if len(filterMap) > 0 && filterMap[strings.TrimSpace(parts[0])] || len(filterMap) == 0 {
+			path, err := url.Parse(strings.TrimSuffix(strings.TrimPrefix(parts[2], "("), ")"))
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse URL %s: %w", workspace, err)
+			}
+			paths = append(paths, *path)
+		}
+	}
+
+	return paths, nil
+}
+
+// ProjectTargets loads the members from the project workspace
+func (c ScarbRunner) ProjectTargets(srcDir string) ([]string, error) {
+	m, err := c.fetchScarbMetadata(srcDir)
+	if err != nil {
+		return []string{}, fmt.Errorf("unable to load cargo metadata\n%w", err)
+	}
+
+	filterMap := c.makeFilterMap()
+
+	workspaces := []string{}
+	for _, workspace := range m.WorkspaceMembers {
+		// This is OK because the workspace member format is `package-name package-version (url)` and
+		//   none of name, version or URL may contain a space & be valid
+		parts := strings.SplitN(workspace, " ", 3)
+		if len(filterMap) > 0 && filterMap[strings.TrimSpace(parts[0])] || len(filterMap) == 0 {
+			workspaces = append(workspaces, workspace)
+		}
+	}
+
+	var names []string
+	for _, pkg := range m.Packages {
+		for _, workspace := range workspaces {
+			if pkg.ID == workspace {
+				for _, target := range pkg.Targets {
+					for _, kind := range target.Kind {
+						if kind == "lib" && strings.HasPrefix(target.SrcPath, srcDir) {
+							names = append(names, target.Name)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return names, nil
+}
+
+// CleanScarbHomeCache clears out unnecessary files from under $CARGO_HOME
+func (c ScarbRunner) CleanScarbHomeCache() error {
+	files, err := os.ReadDir(c.ScarbHome)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return fmt.Errorf("unable to read directory\n%w", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() && file.Name() == "bin" ||
+			file.IsDir() && file.Name() == "registry" ||
+			file.IsDir() && file.Name() == "git" {
+			continue
+		}
+		err := os.RemoveAll(filepath.Join(c.ScarbHome, file.Name()))
+		if err != nil {
+			return fmt.Errorf("unable to remove files\n%w", err)
+		}
+	}
+
+	registryDir := filepath.Join(c.ScarbHome, "registry")
+	files, err = os.ReadDir(registryDir)
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("unable to read directory\n%w", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() && file.Name() == "index" ||
+			file.IsDir() && file.Name() == "cache" {
+			continue
+		}
+		err := os.RemoveAll(filepath.Join(registryDir, file.Name()))
+		if err != nil {
+			return fmt.Errorf("unable to remove files\n%w", err)
+		}
+	}
+
+	gitDir := filepath.Join(c.ScarbHome, "git")
+	files, err = os.ReadDir(gitDir)
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("unable to read directory\n%w", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() && file.Name() == "db" {
+			continue
+		}
+		err := os.RemoveAll(filepath.Join(gitDir, file.Name()))
+		if err != nil {
+			return fmt.Errorf("unable to remove files\n%w", err)
+		}
+	}
+
+	return nil
+}
+
+// ScarbVersion returns the version of cargo installed
+func (c ScarbRunner) ScarbVersion() (string, error) {
+	buf := &bytes.Buffer{}
+
+	if err := c.Executor.Execute(effect.Execution{
+		Command: "cargo",
+		Args:    []string{"version"},
+		Stdout:  buf,
+		Stderr:  buf,
+	}); err != nil {
+		return "", fmt.Errorf("error executing 'cargo version':\n Combined Output: %s: \n%w", buf.String(), err)
+	}
+
+	s := strings.SplitN(strings.TrimSpace(buf.String()), " ", 3)
+	return s[1], nil
+}
+
+// BuildArgs will build the list of arguments to pass `cargo install`
+func (c ScarbRunner) BuildArgs(destLayer libcnb.Layer, defaultMemberPath string) ([]string, error) {
+	envArgs, err := FilterInstallArgs(c.ScarbInstallArgs)
+	if err != nil {
+		return nil, fmt.Errorf("filter failed: %w", err)
+	}
+
+	args := []string{"install"}
+	args = append(args, envArgs...)
+	args = append(args, "--color=never", fmt.Sprintf("--root=%s", destLayer.Path))
+	args = AddDefaultPath(args, defaultMemberPath)
+
+	args, err = AddDefaultTargetForTinyOrStatic(args, c.Stack, c.StaticType)
+	if err != nil {
+		return []string{}, fmt.Errorf("unable to add default target\n%w", err)
+	}
+
+	return args, nil
+}
+
+// FilterInstallArgs provides a clean list of allowed arguments
+func FilterInstallArgs(args string) ([]string, error) {
+	argwords, err := shellwords.Parse(args)
+	if err != nil {
+		return nil, fmt.Errorf("parse args failed: %w", err)
+	}
+
+	var filteredArgs []string
+	skipNext := false
+	for _, arg := range argwords {
+		if skipNext {
+			skipNext = false
+			continue
+		}
+		if arg == "--root" || arg == "--color" {
+			skipNext = true
+			continue
+		}
+		if strings.HasPrefix(arg, "--root=") || strings.HasPrefix(arg, "--color=") {
+			continue
+		}
+		filteredArgs = append(filteredArgs, arg)
+	}
+
+	return filteredArgs, nil
+}
+
+// AddDefaultPath will add --path=. if --path is not set
+func AddDefaultPath(args []string, defaultMemberPath string) []string {
+	for _, arg := range args {
+		if arg == "--path" || strings.HasPrefix(arg, "--path=") {
+			return args
+		}
+	}
+	return append(args, fmt.Sprintf("--path=%s", defaultMemberPath))
+}
+
+// AddDefaultTargetForTinyOrStatic will add the appropriate options if not already set
+func AddDefaultTargetForTinyOrStatic(args []string, stack string, staticType string) ([]string, error) {
+	if !libpak.IsTinyStack(stack) && !libpak.IsStaticStack(stack) {
+		return args, nil
+	}
+
+	// user already picked a target, back off
+	for _, arg := range args {
+		if arg == "--target" || strings.HasPrefix(arg, "--target=") {
+			return args, nil
+		}
+	}
+
+	// user set flags to do a static build, back off
+	rustFlags := os.Getenv("RUSTFLAGS")
+	if strings.Contains(rustFlags, "target-feature=+crt-static") {
+		return args, nil
+	}
+
+	target := "--target=x86_64-unknown-linux-musl"
+	if staticType == StaticTypeGNULIBC {
+		target = "--target=x86_64-unknown-linux-gnu"
+
+		rustFlagsList := []string{}
+		if len(rustFlags) > 0 {
+			rustFlagsList = append(rustFlagsList, rustFlags)
+		}
+		rustFlagsList = append(rustFlagsList, "-C target-feature=+crt-static")
+		newRustFlags := strings.Join(rustFlagsList, " ")
+
+		err := os.Setenv("RUSTFLAGS", newRustFlags)
+		if err != nil {
+			return []string{}, fmt.Errorf("unable to set env RUSTFLAGS to [%s]\n%w", newRustFlags, err)
+		}
+	}
+
+	return append(args, target), nil
+}
+
+func (c ScarbRunner) fetchScarbMetadata(srcDir string) (metadata, error) {
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+
+	if err := c.Executor.Execute(effect.Execution{
+		Command: "cargo",
+		Args:    []string{"metadata", "--format-version=1", "--no-deps"},
+		Dir:     srcDir,
+		Stdout:  &stdout,
+		Stderr:  &stderr,
+	}); err != nil {
+		return metadata{}, fmt.Errorf("unable to read metadata: \n%s\n%s\n%w", &stdout, &stderr, err)
+	}
+
+	var m metadata
+	if err := json.Unmarshal(stdout.Bytes(), &m); err != nil {
+		return metadata{}, fmt.Errorf("unable to parse Scarb metadata: %w", err)
+	}
+
+	return m, nil
+}
+
+func (c ScarbRunner) makeFilterMap() map[string]bool {
+	filter := c.ScarbWorkspaceMembers != ""
+	filterMap := make(map[string]bool)
+	if filter {
+		if !strings.Contains(c.ScarbWorkspaceMembers, ",") {
+			filterMap[c.ScarbWorkspaceMembers] = true
+		}
+		for _, f := range strings.Split(c.ScarbWorkspaceMembers, ",") {
+			filterMap[strings.TrimSpace(f)] = true
+		}
+	}
+
+	return filterMap
+}
--- scarb/scarb_init.go
+++ scarb/scarb_init.go
@@ -1,13 +1,18 @@
 package scarb
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"github.com/buildpacks/libcnb"
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/paketo-buildpacks/libpak/bard"
 	"github.com/paketo-buildpacks/libpak/sherpa"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type ScarbInit struct {
@@ -26,7 +31,6 @@ func NewScarbInit(dependency libpak.BuildpackDependency, cache libpak.Dependency
 
 func (r ScarbInit) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 	r.LayerContributor.Logger = r.Logger
-
 	if err := os.Setenv("PATH", sherpa.AppendToEnvVar("PATH", ":", filepath.Join(layer.Path, "bin"))); err != nil {
 		return libcnb.Layer{}, fmt.Errorf("unable to set $PATH\n%w", err)
 	}
@@ -39,7 +43,9 @@ func (r ScarbInit) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 		if err := sherpa.CopyFile(artifact, file); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to copy %s to %s\n%w", artifact.Name(), file, err)
 		}
-
+		if err := r.DeCompress(filepath.Join(filepath.Dir(file), "scarb-v2.4.3-x86_64-unknown-linux-musl.tar.gz"), filepath.Dir(file)); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to decompress %s to %s\n%w", artifact.Name(), file, err)
+		}
 		if err := os.Chmod(file, 0755); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to chmod %s\n%w", file, err)
 		}
@@ -50,4 +56,60 @@ func (r ScarbInit) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 
 func (r ScarbInit) Name() string {
 	return r.LayerContributor.LayerName()
+}
+func (r ScarbInit) DeCompress(tarFile, dest string) error {
+	srcFile, err := os.Open(tarFile)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	gr, err := gzip.NewReader(srcFile)
+	if err != nil {
+		return err
+	}
+	defer gr.Close()
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+		filename := dest + hdr.Name
+		file, err := createFile(filename)
+		if err != nil {
+			log.Println("create file err = ", err)
+			return err
+		}
+		_, err = io.Copy(file, tr)
+		if err != nil {
+			log.Println("io copy err= ", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func createFile(name string) (*os.File, error) {
+	if ok := Exists(string([]rune(name)[0:strings.LastIndex(name, "/")])); !ok {
+		err := os.MkdirAll(string([]rune(name)[0:strings.LastIndex(name, "/")]), 0755)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return os.Create(name)
+}
+func Exists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
 }
