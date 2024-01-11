ea3ddd547f97f5c35ea647557e35ead6f4788781
zouyuxuan <1311226127@qq.com>
2024-01-11 01:51:10 -0800

    add PATH

--- scarb/scarb_init.go
+++ scarb/scarb_init.go
@@ -1,21 +1,18 @@
 package scarb
 
 import (
-	"archive/tar"
-	"compress/gzip"
 	"fmt"
 	"github.com/buildpacks/libcnb"
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/paketo-buildpacks/libpak/bard"
+	"github.com/paketo-buildpacks/libpak/crush"
 	"github.com/paketo-buildpacks/libpak/sherpa"
-	"io"
-	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 type ScarbInit struct {
+	Version          string
 	LayerContributor libpak.DependencyLayerContributor
 	Logger           bard.Logger
 }
@@ -27,26 +24,30 @@ func NewScarbInit(dependency libpak.BuildpackDependency, cache libpak.Dependency
 		Build:  true,
 	})
 	return ScarbInit{
+		Version:          dependency.Version,
 		LayerContributor: contributor,
 	}
 }
 
 func (r ScarbInit) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 	r.LayerContributor.Logger = r.Logger
-	if err := os.Setenv("PATH", sherpa.AppendToEnvVar("PATH", ":", filepath.Join(layer.Path, "bin"))); err != nil {
-		return libcnb.Layer{}, fmt.Errorf("unable to set $PATH\n%w", err)
-	}
 
 	return r.LayerContributor.Contribute(layer, func(artifact *os.File) (libcnb.Layer, error) {
 		file := filepath.Join(layer.Path, "bin", filepath.Base(artifact.Name()))
 
 		r.Logger.Bodyf("Copying to %s", filepath.Dir(file))
 		// 解压到PATH
-		if err := r.extractTarGz(artifact.Name(), filepath.Dir(file)); err != nil {
-			return libcnb.Layer{}, fmt.Errorf("unable to decompress %s to %s\n%w", artifact.Name(), file, err)
+		if err := crush.Extract(artifact, file, 0); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to expand %s\n%w", artifact.Name(), err)
 		}
 		if err := os.Chmod(filepath.Dir(file), 0755); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to chmod %s\n%w", file, err)
+		}
+
+		scarbPath := filepath.Join(file, fmt.Sprintf("scarb-%s-x86_64-unknown-linux-musl/bin/scarb", r.Version))
+		
+		if err := os.Setenv("PATH", sherpa.AppendToEnvVar("PATH", ":", scarbPath)); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to set $PATH\n%w", err)
 		}
 		return layer, nil
 	})
@@ -54,65 +55,4 @@ func (r ScarbInit) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 
 func (r ScarbInit) Name() string {
 	return r.LayerContributor.LayerName()
-}
-func (r ScarbInit) extractTarGz(src, dest string) error {
-	// 打开 tar.gz 文件
-	file, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// 创建 Gzip 读取器
-	gzipReader, err := gzip.NewReader(file)
-	if err != nil {
-		return err
-	}
-	defer gzipReader.Close()
-
-	// 创建 Tar 读取器
-	tarReader := tar.NewReader(gzipReader)
-
-	// 逐个文件解压
-	for {
-		header, err := tarReader.Next()
-
-		switch {
-		case err == io.EOF:
-			// 到达文件末尾
-			return nil
-		case err != nil:
-			// 发生其他错误
-			return err
-		case header == nil:
-			// 忽略空文件
-			continue
-		}
-
-		// 获取目标文件路径
-		targetFilePath := filepath.Join(dest, header.Name)
-
-		// 确保目标路径存在
-		if strings.HasSuffix(header.Name, "/") {
-			if err := os.MkdirAll(targetFilePath, 0755); err != nil {
-				return err
-			}
-			continue
-		}
-		// 创建或截断目标文件
-		targetFile, err := os.Create(targetFilePath)
-		if err != nil {
-			log.Println("targetFilePath ======", targetFilePath)
-			return err
-		}
-		defer targetFile.Close()
-
-		os.Chmod(targetFile.Name(), os.FileMode(0111))
-		if strings.HasSuffix(targetFile.Name(), "scarb") {
-			log.Println("targetFile.Name() =============", filepath.Join(dest, "scarb"))
-			if err := sherpa.CopyFile(targetFile, filepath.Join(dest, "scarb")); err != nil {
-				return err
-			}
-		}
-	}
 }
