0f6b015457166fe1d0e415ef9c3ecacac3db0330
zouyuxuan <1311226127@qq.com>
2024-01-12 22:12:05 +0800

    add licence

--- scarb/build.go
+++ scarb/build.go
@@ -1,6 +1,8 @@
 /*
  * Copyright 2018-2020 the original author or authors.
  *
+ * COPY FROM amp-buildpacks/scarb.git
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
